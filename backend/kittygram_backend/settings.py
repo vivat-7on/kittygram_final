ef2e2b6a0b0e64455f6eb3246a1e9a6e3f2cfb3c
Semenov Valerii <vivat-7on@yandex.ru>
2024-02-10 16:13:07 +0300

    Fix for settings.py2.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django_db'),
         'USER': os.getenv('POSTGRES_USER', 'django_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
-        'HOST': os.getenv('DB_HOST', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
