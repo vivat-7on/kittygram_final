b98701ea73ffea67b55f6b1e51783f67690c0a9c
Semenov Valerii <vivat-7on@yandex.ru>
2024-02-11 11:54:14 +0300

    Test collectstatic7.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,10 +96,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/backend_static/'
+STATIC_ROOT = '/backend_static/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
